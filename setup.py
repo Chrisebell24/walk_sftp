9eaed01cdd3662089fa70060a6a43fc4d3416140
Chrisebell24 <31806186+Chrisebell24@users.noreply.github.com>
2021-01-08 08:01:49 -0600

    Changed to add src files

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setup(
         "Operating System :: OS Independent",
     ],
     name='walk_sftp',
-    version='0.0.3',
+    version='0.0.4',
     description='Download SFTP files using a glob to get all files & also keep a log to keep track of processing',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    py_modules=[],
+    py_modules=['walk_sftp','util_walk_sftp'],
     package_dir={'': 'src'},
     install_requires = [
         "numpy>=1.16.1",
