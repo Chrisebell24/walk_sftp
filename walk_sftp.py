489ec8897ce3199f16e4026b9e28db894c02b942
Chrisebell24 <31806186+Chrisebell24@users.noreply.github.com>
2021-01-08 07:42:46 -0600

    Changed name of walk sftp file and added test that it exists

--- walk_sftp.py
+++ walk_sftp.py
@@ -15,12 +15,8 @@ from socket import error as socket_error
 from logging import log
 warnings.filterwarnings('ignore')
 
-class _FastTransport(paramiko.Transport):
-    def __init__(self, sock):
-        super(_FastTransport, self).__init__(sock)
-        self.window_size = 2147483647
-        self.packetizer.REKEY_BYTES = pow(2, 40)
-        self.packetizer.REKEY_PACKETS = pow(2, 40)
+
+from src.util_walk_sftp import _FastTransport
 
 class WalkSFTP:
     
