471f607a868226b468a910147293549615c66194
Chris.E.Bell24@gmail.com <Chris.E.Bell24@gmail.com>
2021-03-01 03:06:22 +0000

    added file reference to correctly import test

--- test_walk_sftp.py
+++ test_walk_sftp.py
@@ -1,3 +1,8 @@
+import sys
+import os
+fp = os.path.dirname( os.path.abspath( __file__))
+fp = os.path.join(fp, 'src')
+sys.path.insert(0, fp)
 from walk_sftp import WalkSFTP
 from walk_ftp import WalkFTP
 
